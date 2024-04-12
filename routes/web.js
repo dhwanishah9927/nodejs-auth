db32027410981561858b79d6423db0cf7f663c7e
dhwanishah9927 <83601581+dhwanishah9927@users.noreply.github.com>
2024-04-12 07:05:34 +0530

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,5 @@
-
+// This code defines routes for user authentication and related actions using Express.js. 
+// It includes routes for signing in, signing up, logging out, resetting password, and accessing the home page, with middleware isLoggedIn ensuring authentication for accessing the home route.
 const authController = require("../app/controller/authController")
 const isLoggedIn = require('../app/middleware/guest')
 function initRoutes(app){
