1cc19dcf0669ac78a95835c9ae0202824b4343af
dhwanishah9927 <83601581+dhwanishah9927@users.noreply.github.com>
2024-04-12 07:09:25 +0530

    Update guest.js

--- app/middleware/guest.js
+++ app/middleware/guest.js
@@ -1,3 +1,4 @@
+// This middleware function checks if the user is authenticated and redirects to the sign-in page if not.
 function isLoggedIn(req, res, next) {
         if (req.isAuthenticated()) {
             return next();
